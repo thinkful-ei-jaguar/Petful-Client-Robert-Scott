80ff693bc505a2f850d478d7d189d6e1f19bb644
Alex <robert.perez6@g.austincc.edu>
2020-03-13 21:14:49 -0500

    added some for server config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-    REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || "http://localhost:8000"
+    REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || "https://polar-tundra-16090.herokuapp.com"
 }
+//https://polar-tundra-16090.herokuapp.com
+//"http://localhost:8000"
